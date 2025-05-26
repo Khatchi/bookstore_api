a9613367a24f19f608a4bd8ba1f0b0667f4d59e1
Khatchi <solomon9ice2014@gmail.com>
2025-05-26 22:48:57 +0100

    bookviewset created

--- books/views.py
+++ books/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        genre = self.request.query_param('genre')
+        language = self.request.query_param('language')
+
+        if genre:
+            queryset = queryset.filter(genre__iexact=genre)
+        if language:
+            queryset = queryset.filter(genre__iexact=language)
+
+        return queryset
+
+    
